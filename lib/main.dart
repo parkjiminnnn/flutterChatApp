242d6d0fbc257e84b42f3dba2c3f398321fef2db
박지민 <pjm5489@naver.com>
2024-07-09 00:12:01 +0900

    중도하차 고민중
    중도하차 고민중

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,22 @@
 import 'package:chatapp/screens/main_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Chatting app',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: LoginSignupScreen(),
+      home: const LoginSignupScreen(),
     );
   }
 }
